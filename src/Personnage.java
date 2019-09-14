645ce7ce15965d3618ca72d61a615c5f77dcf0ab
NworkYakzI <49496185+NworkYakzI@users.noreply.github.com>
2019-09-14 19:05:36 -0400

    Construction de la classe Personnage (declaration, setter and getter, constructeur)

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,4 +1,49 @@
 
 public class Personnage {
-
+	
+	//Déclaration des variables de la classe
+	private int attaque,defense,pointsDeVie,initiative;
+	private String nom;
+	
+	//setters and getters
+	public int getAttaque() {
+		return attaque;
+	}
+	public void setAttaque(int attaque) {
+		this.attaque = attaque;
+	}
+	public int getDefense() {
+		return defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	public int getPointsDeVie() {
+		return pointsDeVie;
+	}
+	public void setPointsDeVie(int pointsDeVie) {
+		this.pointsDeVie = pointsDeVie;
+	}
+	public int getInitiative() {
+		return initiative;
+	}
+	public void setInitiative(int initiative) {
+		this.initiative = initiative;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	//Constructeurs
+	public Personnage(int attaque,int defense, int pointsDeVie,int initiative, String nom){
+		this.attaque=attaque;
+		this.defense=defense;
+		this.pointsDeVie=pointsDeVie;
+		this.initiative=initiative;
+		this.nom=nom;
+	}
+	
 }
